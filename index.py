103d19c5a398fdf375d8007976d8eefe7a1c8672
kharleanncf <kharleann@gmail.com>
2021-08-21 18:11:00 -0400

    port hotfix

--- index.py
+++ index.py
@@ -503,8 +503,8 @@ if __name__ == "__main__":
         time=datetime.time(hour=6, minute=00, second=00),
     )
     PORT = 443
-    #updater.start_polling()
-    HEROKULINK = os.getenv("HEROKULINK")
-    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-    updater.bot.setWebhook(HEROKULINK+TOKEN)
+    updater.start_polling()
+    #HEROKULINK = os.getenv("HEROKULINK")
+    #updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    #updater.bot.setWebhook(HEROKULINK+TOKEN)
     updater.idle()
